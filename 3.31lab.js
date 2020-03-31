a930b4e2760190076dec81bfa31162e664b9d6a0
unknown <dburtonthompson@gmail.com>
2020-03-31 15:54:37 -0400

    Slideshow

---
+++ 3.31lab.js
@@ -0,0 +1,30 @@
+var slideshow = {
+	photoList: ['photo1','photo2', 'photo3', 'photo4','photo5'],
+	currentPhotoIndex: 0,
+	nextPhoto: function(){
+	
+		if(this.currentPhotoIndex == (this.photoList.length-1)){
+
+			console.log("End of Slideshow");
+		} 
+		else{
+				this.currentPhotoIndex++;
+		console.log(this.photoList[this.currentPhotoIndex]);
+		}
+	},
+	prevPhoto: function(){
+	
+		if(this.currentPhotoIndex == 0){
+
+			console.log("End of Slideshow");
+		} 
+		else{
+				this.currentPhotoIndex--;
+		console.log(this.photoList[this.currentPhotoIndex]);
+		}
+	},
+	getCurrentPhoto: function(currentPhotoIndex){
+		return currentPhotoIndex
+
+	}
+};
